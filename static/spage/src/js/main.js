a1a0724e71ab555bfafcd7ac9471398cc551b888
fangge <mickshifangge@gmail.com>
2018-02-28 18:25:27 +0800

    修改首页相关bug

--- static/spage/src/js/main.js
+++ static/spage/src/js/main.js
@@ -783,6 +783,22 @@
 
     });
 
+    /* Inifnite Scroll
+   -------------------------------------------------------*/
+    $portfolio.infinitescroll({
+            navSelector  : '#page_nav',    // selector for the paged navigation
+            nextSelector : '#page_nav a',  // selector for the NEXT link (to page 2)
+            itemSelector : '.work-item',     // selector for all items you'll retrieve
+            loading: {
+                finishedMsg: 'No more pages to load.',
+                img: 'http://i.imgur.com/qkKy8.gif'
+            }
+        },
+        // call Isotope as a callback
+        function( newElements ) {
+            $container.isotope( 'appended', $( newElements ) );
+        }
+    );
 
     /* Masonry
     -------------------------------------------------------*/
